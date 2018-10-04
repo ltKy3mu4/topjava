e8dde804fc604111a35a33806709a97f2d8a2f74
ltKy3mu4 <eirogozinnikov@gmail.com>
2018-10-04 21:18:44 +0300

    Finished HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,9 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,13 +21,49 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        List<UserMealWithExceed> somelist= getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        List<UserMealWithExceed> list = new ArrayList<>();
+        List<UserMealWithExceed> buffer = new ArrayList<>();
+        int tempraryCalories = 0;
+
+        LocalDateTime ldt = null;
+
+
+        for (UserMeal um: mealList){
+            if (ldt==null){
+                ldt = um.getDateTime();
+            }
+            else {
+                if (um.getDateTime().toLocalDate().isAfter(ldt.toLocalDate())){
+                    checkCalories(caloriesPerDay, buffer, tempraryCalories);
+                    buffer.clear();
+                    tempraryCalories = 0;
+                    ldt = um.getDateTime();
+                }
+            }
+            tempraryCalories+=um.getCalories();
+            UserMealWithExceed umwe = new UserMealWithExceed(um.getDateTime(),um.getDescription(),um.getCalories(),false);
+            buffer.add(umwe);
+            if (um.getDateTime().toLocalTime().isAfter(startTime)
+                    &&
+                    um.getDateTime().toLocalTime().isBefore(endTime)){
+                list.add(umwe);
+            }
+
+        }
+        checkCalories(caloriesPerDay, buffer, tempraryCalories);
+        return list;
+    }
+
+    private static void checkCalories(int caloriesPerDay, List<UserMealWithExceed> buffer, int tempraryCalories) {
+        if (tempraryCalories>caloriesPerDay){
+            for(UserMealWithExceed meal:buffer){
+                meal.setExceed(true);
+            }
+        }
     }
 }
